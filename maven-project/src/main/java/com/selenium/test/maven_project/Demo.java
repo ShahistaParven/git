3bea184426726ad5f6e14838d89a3703f9385415
ITSUPPORT <ITSUPPORT@MDTR07.mlad.in>
2019-07-04 16:41:44 +0530

    First Commit

---
+++ maven-project/src/main/java/com/selenium/test/maven_project/Demo.java
@@ -0,0 +1,20 @@
+package com.selenium.test.maven_project;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+	public static void main(String[] args) throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver","C:\\Chrome Driver\\chromedriver.exe");// set the driver and set path
+		WebDriver driver = new ChromeDriver();// initialize browser
+		driver.get("https://connect.maveric-systems.com/index.php/site/login");
+		String actualTitle=driver.getTitle();
+		System.out.println(actualTitle);
+		driver.findElement(By.id("LoginForm_username")).sendKeys("sadatk");
+		driver.findElement(By.id("LoginForm_password")).sendKeys("sharukh@111");
+		WebElement ele= driver.findElement(By.name("yt0"));
+		ele.click();
+}
+}
